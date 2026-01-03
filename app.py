c97f1a13bc600b7f0e29fb33af38b71cfe545454
Aman19960110 <amanyadav1004@gmail.com>
2026-01-03 13:19:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
+st.set_page_config(layout="wide")
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+sheet = client.open("Trade_Journal").worksheet("trades")
+
+df = pd.DataFrame(sheet.get_all_records())
